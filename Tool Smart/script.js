dc0681c51fa3d256e849d40d48c279fb82da305a
Teleisha Hall <nickyb63@hotmail.com>
2016-04-23 21:51:10 -0400

    created nav bar and added tools data page

--- Tool Smart/script.js
+++ Tool Smart/script.js
@@ -1,2 +1,11 @@
-﻿document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " Teleisha Hall. All rights reserved.</p>";
+﻿// add navigation bar to HTML pages
+document.getElementById("nav01").innerHTML =
+    "<ul id='menu'>" +
+    "<li><a href='index.html'>Home</a></li>" +
+    "<li><a href='about.html'>About</a></li>" +
+    "<li><a href='tools.html'>Tools</a></li>" +
+    "</ul>";
+
+// add footer to HTML pages
+document.getElementById("foot01").innerHTML = 
+    "<p>&copy;  " + new Date().getFullYear() + " Teleisha Hall. All rights reserved.</p>";
